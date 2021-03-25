e1ec24c39403ea0a38c338a8dac348d7ac9d2c2d
Jose Fernando Resendiz Lopez <gerbo67@outlook.es>
2021-03-24 20:24:40 -0600

    Slider
    Se pusieron los breakpoints de forma mejor, se acomodo los estilos de los sliders de venta.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,34 @@ $(function() {
     $('.your-class').slick({
         infinite: true,
         speed: 300,
-        slidesToShow: 2,
+        slidesToShow: 4,
         slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 });
 
