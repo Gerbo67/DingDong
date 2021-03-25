f236f16dbf820858dfd5a3b439a6ec1386637c65
Jose Fernando Resendiz Lopez <gerbo67@outlook.es>
2021-03-25 17:05:07 -0600

    Slider API
    Se agrego el consumo de API dentro del sitio para los sliders

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,5 @@
 $(function() {
-    $('.your-class').slick({
-        infinite: true,
-        speed: 300,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-          ]
-    });
+    LlenadoSlider();
 });
 
 
@@ -63,3 +32,109 @@ $('.github').click(function (e){
     window.location.href="https://github.com/Gerbo67/DingDong";
     console.log(1);
 });
+
+
+function LlenadoSlider(){
+    var res = "";
+
+    $.ajax({
+        url: "https://dingdongapi.azurewebsites.net/api/marcas",
+        type: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (MD) {
+            for(i = 0; i<MD.length;i++){
+                res += '<div class="sector">' +
+                            '<div class="your-class">' +
+                                 '<div class="cardShop">' +
+                                     '<div class="cardss">' +
+                                        '<div class="cardLogo">' +
+                                            '<img class="MarcaLogo" src="assets/img/Logos/SterenLogo.png" alt="Steren Logo">' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>';
+                //----------------------------------Items---------------------------------------//
+                console.log(MD[i].Id);
+                $.ajax({
+                    url: "https://dingdongapi.azurewebsites.net/api/items/"+MD[i].Id,
+                    async:false,
+                    type: "GET",
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    success: function (data) {
+                        for(c = 0; c<data.length;c++){
+
+                            res +='<div class="cardShop">' +
+                                    '<div class="cardss">' +
+                                        '<div class="headerCard">' +
+                                            '<img class="ItemImage" src="assets/img/Items/Steren1.png" alt="Timbres">' +
+                                        '</div>' +
+                                        '<div class="bodyCard">' +
+                                            '<h1>'+ data[c].ItemNombre +'</h1>' +
+                                             '<p>' + data[c].Descripcion +'</p>' +
+                                        '</div>' +
+                                        '<div class="footerCard">' +
+                                            '<div class="footerItems">' +
+                                                '<button class="footerButton" >Agregar al Carrito</button>' +
+                                                    '<div class="footerInfo"></div>' +
+                                                    '<div class="footerPlay"></div>' +
+                                             '</div>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>';
+                        }
+                    },
+                    error: function (XMLHttpRequest, textStatus, errorThrown) {
+                        alert("Error when the system try to generate the general graphic");
+                    }
+                });
+                res += '</div></div>';
+            }
+
+            const param = document.querySelector('.sliderShop');
+
+            param.innerHTML += res;
+
+            slick();
+
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Error when the system try to generate the general graphic");
+        }
+    });
+}
+
+function slick(){
+    $('.your-class').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+}
